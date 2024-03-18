4d1675ded812d769df0d90d90122e7f17ba732b0
Mathspy <mathspy257@gmail.com>
2024-03-18 19:16:33 -0400

    ZOOOOOOOOM

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
 use bevy::{
-    app::{App, Startup},
+    app::{App, Startup, Update},
     asset::{Asset, Assets},
-    core_pipeline::core_2d::Camera2dBundle,
-    ecs::system::{Commands, ResMut},
-    math::primitives::Rectangle,
+    core_pipeline::core_2d::{Camera2d, Camera2dBundle},
+    ecs::{
+        query::With,
+        system::{Commands, Local, Query, ResMut, Resource},
+    },
+    math::{primitives::Rectangle, vec3, Vec2},
     reflect::TypePath,
     render::{color::Color, mesh::Mesh, render_resource::AsBindGroup},
     sprite::{Material2d, Material2dPlugin, MaterialMesh2dBundle, Mesh2dHandle},
+    transform::components::Transform,
     DefaultPlugins,
+};
+use bevy_egui::{
+    egui::{self, Slider},
+    EguiContexts, EguiPlugin,
 };
 
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, Material2dPlugin::<SdfCircle>::default()))
+        .add_plugins(EguiPlugin)
         .add_systems(Startup, setup)
+        .add_systems(Update, render_ui)
         .run();
 }
 
@@ -34,13 +44,44 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<SdfCircle>>,
 ) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_scale(vec3(1.0, 1.0, 1.0)),
+        ..Default::default()
+    });
 
     commands.spawn(MaterialMesh2dBundle {
         mesh: Mesh2dHandle(meshes.add(Rectangle::new(100.0, 100.0))),
+        transform: Transform::from_xyz(0.0, -50.0, 0.0),
         material: materials.add(SdfCircle {
             color: Color::rgb_linear(1., 1., 0.),
         }),
         ..Default::default()
     });
+}
+
+#[derive(Resource)]
+struct UiState {
+    zoom_level: f32,
+}
+
+impl Default for UiState {
+    fn default() -> Self {
+        UiState { zoom_level: 1.0 }
+    }
+}
+
+fn render_ui(
+    mut state: Local<UiState>,
+    mut contexts: EguiContexts,
+    mut query: Query<&mut Transform, With<Camera2d>>,
+) {
+    egui::Window::new("CIRCLE").show(contexts.ctx_mut(), |ui| {
+        ui.add(Slider::new(&mut state.zoom_level, 1.0..=100.0).text("ZOOOOM!"));
+    });
+
+    let Ok(mut transform) = query.get_single_mut() else {
+        return;
+    };
+
+    transform.scale = Vec2::splat(1.0 / state.zoom_level).extend(1.0);
 }
